5e5590bfad65b78e5b68bc940c0f271710e56c1f
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-22 14:33:45 +0800

    Add calculator and style & hover effect

--- calculator.js
+++ calculator.js
@@ -20,3 +20,18 @@ console.log(`value of add is ${operate(4,"+",4)}`)
 console.log(`value of minus is ${operate(4,"-",4)}`)
 console.log(`value of multiplacation is ${operate(4,"*",4)}`)
 console.log(`value of divide is ${operate(4,operator,4)}`)
+
+
+let numpad = document.getElementById('keyContainer')
+
+let keys = document.getElementsByClassName("keybtn")
+
+keys = Array.from(keys)
+keys.forEach(key => {
+    key.addEventListener('mouseenter',function(){
+        this.classList.toggle('active')
+    })
+    key.addEventListener('mouseleave',function(){
+        this.classList.toggle('active')
+    })
+});
